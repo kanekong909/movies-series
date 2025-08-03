4cca239fd586b3295bedfb1c4e267165a661e4a4
MateoBossa <mateobossa909@gmail.com>
2025-08-03 18:41:38 -0500

    Se añadio recomendadas a index.html

--- script.js
+++ script.js
@@ -52,6 +52,25 @@ async function loadDefaultMovies() {
   renderResults(data.results);
 }
 
+// Recomendadas
+async function loadRecommendedContent() {
+  try {
+    const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=es-ES&sort_by=popularity.desc`);
+    const data = await res.json();
+    const recomendados = data.results.slice(0, 10);
+
+    const contenedor = document.getElementById('recomendados-container');
+    contenedor.innerHTML = recomendados.map(item => `
+      <div class="card-recomendado" onclick="redirigirADetalles('${item.id}', 'movie')">
+        <img src="https://image.tmdb.org/t/p/w300${item.poster_path}" alt="${item.title}">
+        <p>${item.title}</p>
+      </div>
+    `).join('');
+  } catch (error) {
+    console.error('Error al cargar recomendados:', error);
+  }
+}
+
 // Busqueda
 async function search(query) {
   const res = await fetch(`https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=es-ES&query=${encodeURIComponent(query)}`);
@@ -104,4 +123,5 @@ moreInfoBtn.addEventListener('click', () => {
 // Llamar a la funcion cargar por defecto las peliculas cuando se carga la pagina
 window.addEventListener('DOMContentLoaded', () => {
   loadDefaultMovies();
+  loadRecommendedContent();
 });
